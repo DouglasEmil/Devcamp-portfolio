b5232692170e983848cec6e855f914ee899dfe8a
Emil Nielsen <emil@douglasnielsen.com>
2019-06-12 17:18:00 +0200

    Add Skills

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_111145) do
+ActiveRecord::Schema.define(version: 2019_06_12_125000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,18 +22,25 @@ ActiveRecord::Schema.define(version: 2019_06_12_111145) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "portfolios", force: :casade do |t|
-    t.string   "title"
-    t.string   "subtitle"
-    t.text     "body"
-    t.text     "main_image"
-    t.text     "thumb_image"
+  create_table "guides", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "guides", force: :cascade do |t|
+  create_table "portfolios", force: :cascade do |t|
     t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
