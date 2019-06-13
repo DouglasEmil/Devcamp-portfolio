a7ee7c441c633c51e1eae44e607e475ad07a4767
Emil Nielsen <emil@douglasnielsen.com>
2019-06-13 18:38:24 +0200

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_125000) do
+ActiveRecord::Schema.define(version: 2019_06_12_165118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "guides", force: :cascade do |t|
     t.string "title"
@@ -22,6 +21,15 @@ ActiveRecord::Schema.define(version: 2019_06_12_125000) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "title"
