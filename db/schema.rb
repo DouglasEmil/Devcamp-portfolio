219b8942e508dc3ee29443211acff88a758587e1
Emil Nielsen <emil@douglasnielsen.com>
2019-06-13 18:16:57 +0200

    delete porfolios.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2019_06_12_125000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "guides", force: :cascade do |t|
     t.string "title"
@@ -22,6 +28,15 @@ ActiveRecord::Schema.define(version: 2019_06_12_125000) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "title"
