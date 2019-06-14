8ed4c5aabafe2a8df25b6b0d2b0eef9c11b27a8f
Emil Nielsen <emil@douglasnielsen.com>
2019-06-14 15:42:49 +0200

    modify shema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 2019_06_12_165118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "guides", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
