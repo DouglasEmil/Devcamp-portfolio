f7812c8eb2a70a6ba27e9d576f640534b01b6964
Emil Nielsen <emil@douglasnielsen.com>
2019-06-17 23:09:41 +0200

    New portfolio create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,4 +6,18 @@ class PortfoliosController < ApplicationController
 	def new
 		@portfolio_item = Portfolio.new
 	end
+
+	def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolio_path, notice: 'Your Portfolio item is now live.' }
+        format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
